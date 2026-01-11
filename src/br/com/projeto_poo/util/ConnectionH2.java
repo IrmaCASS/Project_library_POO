10a46e8608c90aed98b93bc55bdb1e44c1a05a62
IrmaCASS <irmacristinaphb@gmail.com>
2026-01-11 17:27:07 -0300

    Atualizacao do BD

--- src/br/com/projeto_poo/util/ConnectionH2.java
+++ src/br/com/projeto_poo/util/ConnectionH2.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class ConnectionH2 {
     // Configurações do H2 (pode ser em memória ou arquivo)
-    private static final String URL = "jdbc:h2:tcp:/testdb" +
-            "AUTO_SERVER=TRUE;INIT=RUNSCRIPT FROM 'classpath:Banco_de_dados.sql'";
+    private static final String URL = "jdbc:h2:./database/projeto_db;" +
+            "DB_CLOSE_DELAY=-1;INIT=RUNSCRIPT FROM 'classpath:Banco_de_dados.sql'";
     private static final String USER = "sa";
     private static final String PASS = "";
     private static final String DRIVER = "org.h2.Driver";
